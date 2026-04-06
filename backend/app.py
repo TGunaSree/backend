2899590886473966f1e20b77b391bf0c38f0fcf6
TGunaSree <163656211+TGunaSree@users.noreply.github.com>
2026-04-06 17:37:03 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -54,4 +54,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    socketio.run(app, debug=True, port=5000)
+    port = int(os.environ.get("PORT",5000))
+    socketio.run(app, host = "0.0.0.0", port=port)
